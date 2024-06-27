ab3aa9c6f49572246f197a41f8c822ee60d68b79
ripper21 <ripperlakus@gmail.com>
2024-06-27 13:49:53 +0200

    agregar funciones al controlador

--- src/main/java/es/tiendazara/web/controllers/PrendaController.java
+++ src/main/java/es/tiendazara/web/controllers/PrendaController.java
@@ -1,29 +1,44 @@
 package es.tiendazara.web.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-public class PrendaController {
+import es.tiendazara.web.entities.Prenda;
+import es.tiendazara.web.services.prenda.PrendaService;
+import lombok.extern.slf4j.Slf4j;
 
-	@RequestMapping("/prenda")
-	public String miMetodo() {
+@Controller
+@Slf4j
+@RequestMapping(value={"/","/prenda"})
+public class PrendaController {
+	@Autowired	
+	PrendaService servicio ; 
+	
+	@GetMapping
+	public String paginaDepartamentos(Model model) throws Exception {
+		log.info("[paginaDepartamentos]");
+		List<Prenda> prendas  = servicio.listarTodasLasPrendas();
+		
+		model.addAttribute("prendas", prendas);	
 		return "prenda";
 	}
 	
-	// Login form with error
-	  @RequestMapping("/prendaError")
-	  public String loginError(Model model) {
-	    model.addAttribute("loginError", true);
-	    return "prenda";
-	  }
-	  
-	  @RequestMapping("/accesoDenegado")
-		public String accessDenied() {
-			return "accesoDenegado";
-	  }
-	  
+	@GetMapping("/{id}")
+	public String paginaDepartamento(@PathVariable Integer id,Model model) throws Exception {
+		log.info("[paginaDepartamento]");
+		log.debug("[id:"+id+"]");
+		Prenda prenda = servicio.get(id);
+		
+		model.addAttribute("prenda", prenda);	
+		return "prenda";
+	}
+	
+
 
 }
